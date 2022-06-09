769a6e039c3e75f3cc4a5381ea74e233dca6f579
thedark1232 <World1300@yandex.ru>
2022-06-09 16:08:54 +0300

    построение класса ConfigurationSingletons

--- ConfigurationSingleton.cs
+++ ConfigurationSingleton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,23 +9,44 @@ namespace MacTep_9lpJIblkoB_2
 {
     class ConfigurationSingleton
     {
-        private static ConfigurationSingleton configurationSingleton;
-        private string appPath;
-        private string _imagesDirectory;
+        private static ConfigurationSingleton appConfiguration;
 
         public string AppPath { get; private set; }
         public string ImagesDirectory { get; private set; }
         
+        public string BackgroundDirectory { get; private set; }
+        
+        public string LogsDirectory { get; private set; }
+
+        public string ErrorsDirectory { get; private set; }
 
         private ConfigurationSingleton()
         {
             AppPath = Environment.CurrentDirectory;
+            ImagesDirectory = CheckPath(AppPath + @"\Images");
+            BackgroundDirectory = CheckPath(ImagesDirectory + @"\Backgrounds");
+            LogsDirectory = CheckPath(AppPath + @"\Logs");
+            ErrorsDirectory = CheckPath(LogsDirectory + @"\Errors");
         }
 
         public static ConfigurationSingleton GetConfiguration()
         {
-            if(configurationSingleton == null) configurationSingleton = new ConfigurationSingleton();  
-            return configurationSingleton;
+            if(appConfiguration == null) appConfiguration = new ConfigurationSingleton();  
+            return appConfiguration;
+        }
+
+        //проверка пути к папке, если отсутствует, значит создать папку
+        private string CheckPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new Exception("путь имеет значение Null или пуст");
+            }
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            return path;
         }
     }
 }
