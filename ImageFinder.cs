9cb67bc6e082f8511075ed0d3754b9688679a892
thedark1232 <World1300@yandex.ru>
2022-06-09 14:54:12 +0300

    создание класса ImageFinder

---
+++ ImageFinder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace MacTep_9lpJIblkoB_2
+{
+    class ImageFinder
+    {
+        public string[] FindAllImages(string _path)
+        {
+            if (string.IsNullOrEmpty(_path))
+            {
+                throw new Exception("путь для поиска изображений не задан");
+            }
+            if (!Directory.Exists(_path))
+            {
+                throw new Exception("путь не существует");
+            }
+            string[] _imagesArray = Directory.GetFiles(_path, "*.jpg", SearchOption.TopDirectoryOnly);
+            return _imagesArray;
+
+        }
+    }
+}
