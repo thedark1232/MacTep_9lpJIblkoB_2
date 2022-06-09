b932f19f587a03289b85f6458a0a4245482d0d0e
thedark1232 <World1300@yandex.ru>
2022-06-09 15:20:52 +0300

    создание класса ConfigurationSingleton

---
+++ ConfigurationSingleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MacTep_9lpJIblkoB_2
+{
+    class ConfigurationSingleton
+    {
+        private static ConfigurationSingleton configurationSingleton;
+        private string appPath;
+        private string _imagesDirectory;
+
+        public string AppPath { get; private set; }
+        public string ImagesDirectory { get; private set; }
+        
+
+        private ConfigurationSingleton()
+        {
+            AppPath = Environment.CurrentDirectory;
+        }
+
+        public static ConfigurationSingleton GetConfiguration()
+        {
+            if(configurationSingleton == null) configurationSingleton = new ConfigurationSingleton();  
+            return configurationSingleton;
+        }
+    }
+}
